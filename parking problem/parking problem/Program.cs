7d72174458dd6031d09c0002b3e0b41dc9f2ce7c
Raphael MOUROT-PELADE <raphael.mourot-pelade@epita.fr>
2021-02-22 16:35:39 +0100

    commit

---
+++ parking problem/parking problem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace parking_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            customer popo = new customer(5,1);
+            customer pupu = new customer(24,2);
+            customer papa = new customer(18 ,3);
+            
+            
+            Console.WriteLine(papa.CalculateCharges());
+        }
+    }
+}
---
+++ parking problem/parking problem/customer.cs
@@ -0,0 +1,34 @@
+﻿namespace parking_problem
+{
+    public class customer
+    {
+        private float parking_time { get; set; }
+        private float parking_charges { get; set; }
+        private int Id { get; set; }
+
+        public customer(float parkingTime, int id)
+        {
+            Id = id;
+            parking_time = parkingTime;
+            parking_charges = 2;
+        }
+
+        public float CalculateCharges()
+        {
+            if (parking_time <= 0 || parking_time > 24)
+            {
+                parking_charges = 0;
+            }
+            if (parking_time > 3)
+            {
+                if (parking_time % 1f != 0f)
+                    parking_time = parking_time % 1 + 1;
+                parking_charges = 2 + (parking_time - 3) * 0.5f;
+            }
+
+            if (parking_charges > 10)
+                parking_charges = 10;
+            return parking_charges;
+        }
+    }
+}
